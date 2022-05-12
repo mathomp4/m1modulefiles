557790f9d63bfa8c99ae27d9a068f1814c6acf30
Matthew Thompson <matthew.thompson@nasa.gov>
2022-05-12 15:58:20 -0400

    Use Baselibs 7.1.0

--- MPI/gcc-gfortran-11.2.0/openmpi-4.1.3/Baselibs/7.1.0.lua
+++ MPI/gcc-gfortran-11.2.0/openmpi-4.1.3/Baselibs/7.1.0.lua
@@ -1,7 +1,7 @@
 -- [[
 --
 -- This was built using:
--- $ make -j6 install ESMF_COMM=openmpi ESMF_COMPILER=gfortran prefix=$HOME/installed/MPI/gcc-gfortran-11.2.0/openmpi-4.1.3/Baselibs/6.2.13/Darwin |& tee makeinstall.gcc-gfortran-11.2.0_openmpi-4.1.3.log
+-- $ make -j3 install ESMF_COMM=openmpi ESMF_COMPILER=gfortran prefix=$HOME/installed/MPI/gcc-gfortran-11.2.0/openmpi-4.1.3/Baselibs/7.1.0/Darwin |& tee makeinstall.gcc-gfortran-11.2.0_openmpi-4.1.3.log
 --
 -- NOTE: To build curl on Parcel, I had to do:
 --
