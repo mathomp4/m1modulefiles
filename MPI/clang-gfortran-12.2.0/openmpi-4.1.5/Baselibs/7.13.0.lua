7af8573a2e39fa7d6f32211b414d45c4f61ca9c2
Matthew Thompson <matthew.thompson@nasa.gov>
2023-07-06 10:33:29 -0400

    Add 7.13.0 Baselibs

---
+++ MPI/clang-gfortran-12.2.0/openmpi-4.1.5/Baselibs/7.13.0.lua
@@ -0,0 +1,48 @@
+-- [[
+--
+-- This was cloned with:
+--
+--   git clone -j 4 --recurse-submodules -b v7.13.0 https://github.com/GEOS-ESM/ESMA-Baselibs.git ESMA-Baselibs-7.13.0/src
+--
+-- This was built using:
+-- $ make -j6 install ESMF_COMM=openmpi ESMF_COMPILER=gfortranclang prefix=$HOME/installed/MPI/clang-gfortran-12.2.0/openmpi-4.1.5/Baselibs/7.13.0/Darwin |& tee makeinstall.clang-gfortran-12.2.0_openmpi-4.1.5.log
+--
+-- NOTE: To build curl on Parcel, I had to do:
+--
+--   brew install automake autoconf libtool
+--
+-- then I had to make symlinks so that it could find these:
+--
+--   ln -s $(brew --prefix)/bin/automake $HOME/bin/automake
+--   ln -s $(brew --prefix)/bin/aclocal $HOME/bin/aclocal
+--   ln -s $(brew --prefix)/bin/glibtool $HOME/bin/libtool
+--
+-- NOTE THE LAST ONE! Brew installs glibtool, so as not to collide with clang libtool
+-- ]]
+
+family("Baselibs")
+--prereq("clang-gfortran/12.2.0", "openmpi/4.1.5")
+
+local compilername = "clang-gfortran-12.2.0"
+local mpiname = "openmpi-4.1.5"
+
+local version = myModuleVersion()
+local pathdir = pathJoin("MPI",compilername,mpiname)
+local homedir = os.getenv("HOME")
+local installdir = pathJoin(homedir,"installed")
+local pkgdir = pathJoin(installdir,pathdir,"Baselibs",version)
+
+-- Setup Modulepath for packages built by this MPI stack
+local mroot = os.getenv("MODULEPATH_ROOT")
+local mdir = pathJoin(mroot,"Baselibs",compilername,mpiname)
+prepend_path("MODULEPATH", mdir)
+
+setenv("BASEDIR",pkgdir)
+setenv("basedir",pkgdir)
+setenv("BASEBIN",pathJoin(pkgdir,"Darwin/bin"))
+
+prepend_path("PATH",pathJoin(pkgdir,"Darwin/bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(pkgdir,"Darwin/lib"))
+prepend_path("DYLD_LIBRARY_PATH",pathJoin(pkgdir,"Darwin/lib"))
+prepend_path("MANPATH",pathJoin(pkgdir,"Darwin/share/man"))
+
