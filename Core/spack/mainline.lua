61294d575ea41504085b51fe94f996682c64c2ac
Matthew Thompson <matthew.thompson@nasa.gov>
2024-06-26 11:21:59 -0400

    Add spack modules (Don't work)

---
+++ Core/spack/mainline.lua
@@ -0,0 +1,12 @@
+-- -*- lua -*-
+
+local homedir = os.getenv("HOME")
+local spackdir = "spack"
+local spackroot = pathJoin(homedir, spackdir)
+setenv("SPACK_ROOT", spackroot)
+source_sh("zsh", pathJoin(spackroot, "share/spack/setup-env.sh"))
+
+-- Also we need to know where modules are installed
+local spackarch = "darwin-sonoma-aarch64"
+local modulepath = pathJoin(spackroot, "share/spack/lmod", spackarch, "Core")
+prepend_path("MODULEPATH", modulepath)
