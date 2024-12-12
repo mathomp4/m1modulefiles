c4d5d3d58ebc29d6ffbe61a8bb99eb4fed88258f
Matthew Thompson <matthew.thompson@nasa.gov>
2024-12-12 12:28:12 -0500

    update geospyd

--- Core/GEOSpyD/24.9.2-0/3.12.lua
+++ Core/GEOSpyD/24.9.2-0/3.12.lua
@@ -18,7 +18,7 @@ local installdir = pathJoin(homedir,"installed/Core/GEOSpyD")
 
 local miniforge_version = "24.9.2-0"
 local python_version = "py3.12"
-local builddate = "2024-11-05"
+local builddate = "2024-11-27"
 
 local pathdir = pathJoin(miniforge_version,builddate,'envs',python_version)
 local pkgdir = pathJoin(installdir,pathdir)
